2a3d5a7ef5b9a3f354cb925781160c4a774ffd8d
kabballero <61820999+kabballero@users.noreply.github.com>
2023-05-07 16:14:27 +0300

    Add files via upload

--- backend-api/server.js
+++ backend-api/server.js
@@ -3,20 +3,34 @@ const express=require('express')
 const app=express()
 const cors=require('cors')
 var mysql=require('mysql');
+const schedule = require('node-schedule');
 const port=9103;
 
 app.use(cors());
-
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}.`);
-  });
 
 var  con = mysql.createPool({
     multipleStatements: true,
     host: "127.0.0.1",
     user:"root",
-    database: "schoolib"
+    database: "library"
 });  
+
+async function deleteOldReservers() {
+    try {
+      const result = con.query('delete from reservation where current_date>duedate');
+      console.log(`Rows deleted`);
+    } catch (err) {
+      console.error(err);
+    } 
+  }
+
+const job = schedule.scheduleJob('0 0 * * *', function() {
+    deleteOldReservers();
+  });
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}.`);
+  });
 
 app.get('/login/:role/:username/:kodikos', (req,res)=>{
     let myquery="select * from "+""+req.params.role+""+" where username="+"'"+req.params.username+"'"+" and kodikos="+"'"+req.params.kodikos+"'";
