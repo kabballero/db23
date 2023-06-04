da58d6d929c3adeb44c825a3536369bd27c03573
kabballero <61820999+kabballero@users.noreply.github.com>
2023-06-04 18:24:04 +0300

    Delete backend-api/data directory

--- backend-api/data/MOcategoydata.sql
+++
@@ -1,3 +0,0 @@
-use library;
-insert into bookCategory values (1,'funny'),
-(2,'mysterious'),(3,'detective'),(4,'horror');
--- backend-api/data/MOoperatordata.sql
+++
@@ -1,6 +0,0 @@
-use library;
-insert into operator (operatorID, schoolID, teacherID) values (1, 1, 1);
-insert into operator (operatorID, schoolID, teacherID) values (2, 2, 2);
-insert into operator (operatorID, schoolID, teacherID) values (3, 3, 3);
-insert into operator (operatorID, schoolID, teacherID) values (4, 4, 4);
-insert into operator (operatorID, schoolID, teacherID) values (5, 5, 5);
